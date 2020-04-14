6b22f658d3d6be7cb28a97272f9aaf3799cae316
preetika <shiva@seconddigital.com>
2020-04-14 14:52:42 +0530

    register a client form done

--- application/views/registration.php
+++ application/views/registration.php
@@ -30,7 +30,7 @@ $this->load->view('inc/header');
 
             td, th {
                 padding: 0;
-                padding: 9px 6px;
+                padding: 9px 6px!important;
             }
     </style>
 
@@ -43,7 +43,11 @@ $this->load->view('inc/header');
           $this->load->view('inc/footer');
 
        ?>
+        <!-- Profile -->
+       <?php
+            $this->load->view('profile');
 
+        ?>
         <div class="page-content">
             <?php
               $this->load->view('inc/collapsable_header');
@@ -83,6 +87,8 @@ $this->load->view('inc/header');
                                       <td><?php echo $data->project_name; ?></td>  
                                       <td><?php echo $data->status_name; ?></td> 
                                       <td class="hidden"><?php echo $data->contact_no1; ?></td>
+                                      <td><button style="cursor:pointer" href="#myModal" data-toggle="modal" data-target="#myModalcall" class="icon icon-xs icon-circle shadow-huge bg-icon"><i class="fa fa-plus"></i></button></td>
+                  
                                      
                                   </tr>
                               <?php $i++; } 
@@ -100,8 +106,8 @@ $this->load->view('inc/header');
                       </div>
                   </div> 
            
-               <div class="col-md-6 form-group">
-                       <!--  <input type="checkbox" name="fancy-checkbox-info" onclick="clientEmail()"  id="fancy-checkbox-info" autocomplete="off" />
+                <!--<div class="col-md-6 form-group">
+                        <input type="checkbox" name="fancy-checkbox-info" onclick="clientEmail()"  id="fancy-checkbox-info" autocomplete="off" />
                         <div class="btn-group">
                             <label for="fancy-checkbox-info" class="btn btn-info">
                                 <span class="glyphicon glyphicon-ok"></span>
@@ -110,8 +116,79 @@ $this->load->view('inc/header');
                             <label for="fancy-checkbox-info" class="btn btn-default active">
                                Client Registration Email
                             </label>
-                        </div> -->
+                        </div> 
                         <div id="clientEmail" hidden>
+                            <div class="col-sm-12 form-group">
+                                <label for="email_id">Email Id:</label>
+                                <input type="email" class="form-control" id="client_email_id" name="email_id" placeholder="Email Id">
+                            </div>
+                            <div class="col-sm-12 form-group">
+                                <label for="subject">Subject:</label>
+                                <input type="text" class="form-control" id="client_email_subject" name="subject" value="Client Registration" placeholder="Subject">
+                            </div>
+                            <div class="col-sm-12 form-group">
+                                <label for="comment">Email Body:</label>
+                                <textarea class="form-control" name="notes" id="client_email_body" rows="15" id="comment">          
+                                    Dear sir / madam,
+
+                                    Greetings From Fullbasket Property...
+
+                                    Kindly register the below client For __________________ project On behalf Of Fullbasket Property 
+
+                                    Property & acknowledge.
+
+                                    Client Name : ________________
+
+                                    Contact No. : ________________
+
+                                    E-mail ID   : ________________
+
+                                    Thanks & Regards
+                                    Team Fullbasket Property
+                                </textarea>
+                            </div>
+                            <div class="col-sm-12 form-group">
+                                <div class="alert alert-success" id="regmail_success" style="display:none">
+                                    <strong>Success!</strong> Email sent successfully.
+                                </div>
+                                <button type="button" onclick="sendRegMail()" class="btn btn-success">Send</button>
+                            </div>
+                        </div>
+                    </div> -->
+            </div>
+            <div class="clearfix"></div>
+
+            </div>
+            <div style="margin-bottom: 60px">
+        </div>
+
+      
+    
+        <div class="menu-hider"></div>
+    </div>
+   
+
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                pagingType: "simple",
+                //"sScrollY": "200px",
+                "sScrollX": true
+            });
+        });
+    </script>
+</body>
+
+  <div class="modal fade" id="myModalcall" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+      
+        <div class="modal-body">
+          <p  style="margin-bottom: 8px; text-align: center;">Register a Client</p>
+          <table>
+          <div id="clientEmail">
                             <div class="col-sm-12 form-group">
                                 <label for="email_id">Email Id:</label>
                                 <input type="email" class="form-control" id="client_email_id" name="email_id" placeholder="Email Id">
@@ -148,53 +225,6 @@ Team Fullbasket Property
                                 <button type="button" onclick="sendRegMail()" class="btn btn-success">Send</button>
                             </div>
                         </div>
-                    </div> 
-            </div>
-            <div class="clearfix"></div>
-
-            </div>
-        </div>
-       <!-- Profile -->
-       <?php
-            $this->load->view('profile');
-
-        ?>
-        <div class="menu-hider"></div>
-    </div>
-   
-
-    <script>
-        $(document).ready(function() {
-            $('#example').DataTable({
-                pagingType: "simple",
-                //"sScrollY": "200px",
-                "sScrollX": true
-            });
-        });
-    </script>
-</body>
-
-  <div class="modal fade" id="myModalcall" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-      
-        <div class="modal-body">
-          <p  style="margin-bottom: 8px; text-align: center;">Call Now.</p>
-          <table>
-            <tr>
-                <th>Customer</th>
-                <th>Number</th>
-            </tr>
-            <tr>
-                <td>abc</td>
-                <td><a href="tel:+91 1234567898"><i class="fas fa-phone color-green1-dark"></i></a></td>
-            </tr>
-            <tr>
-                <td>abc</td>
-                <td><a href="tel:+91 1234567898"><i class="fas fa-phone color-green1-dark"></i></a></td>
-            </tr>
             </table>
             
         </div>
@@ -204,33 +234,3 @@ Team Fullbasket Property
     </div>
   </div>
 
-
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-      
-        <div class="modal-body">
-          <p style="margin-bottom: 8px; text-align: center;">Notes</p>
-          <table>
-            <tr>
-                <th>Read Previos Note</th>
-                <th>Add Notes</th>
-            </tr>
-            <tr>
-                <td><a><i class="fab fa-readme color-blue-dark"></i></a></td>
-                <td><a><i class="fas fa-plus-circle color-green1-dark"></i></a></td>
-            </tr>
-            <tr>
-                <td><a><i class="fab fa-readme color-blue-dark"></i></a></td>
-                <td><a><i class="fas fa-plus-circle color-green1-dark"></i></a></td>
-            </tr>
-          </table>
-            
-        </div>
-       
-      </div>
-      
-    </div>
-  </div>
